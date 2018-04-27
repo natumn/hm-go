0d6b752db4cad301e8850cbe842bf9d6eb90d18a
natumn <natsume.pcuse75@gmail.com>
2018-04-27 15:27:29 +0900

    main修正

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,46 @@
-module Main where
+module Main
+where
 
-import Parser
+import           Parser
 
-import Control.Monad.Trans
-import System.Console.Haskeline
+import           Control.Monad.Trans
+import           System.Console.Haskeline
+import           System.Environment
+import qualified LLVM.AST                      as AST
 
-process :: String -> IO ()
-process line = do
-  let res = parseToplevel line
-  case res of
-      Left err -> print err
-      Right ex -> mapM_ print ex
+initModule :: AST.Module
+initModule = emptyModule "my cool jit"
+
+process :: AST.Module -> String -> IO (Maybe AST.Module)
+process modo source = do
+    let res = parseToplevel source
+    case res of
+        Left  err -> print err >> return Nothing
+        Right ex  -> do
+            ast <- codegen modo ex
+            return $ Just ast
+
+processFile :: String -> IO (Maybe AST.Module)
+processFile fname = readFile fname >>= process initModule
+
+
+repl :: IO ()
+repl = runInputT defaultSettings (loop initModule)
+  where
+    loop mod = do
+        minput <- getInputLine "hli> "
+        case minput of
+            Nothing    -> outputStrLn "Goodbye."
+            Just input -> do
+                modo <- liftIO $ process mod Input
+                case modn of
+                    Nothing   -> loop mod
+                    Just modo -> loop modn
 
 main :: IO ()
-main = runInputT defaultSettings loop
-    where
-    loop = do
-      minput <- getInputLine "hurlei> "
-      case minput of
-          Nothing -> outputStrLn "Goodbye."
-          Just input -> (liftIO $ process input) >> loop
+main = do
+    args <- getArgs
+    case args of
+        []      -> repl
+        [fname] -> processFile fname >> return ()
+
